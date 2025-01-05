2d1665d71156c1cea464e5a27d808dc84b811039
Ross Gardiner <github@public.rossng.eu>
2025-01-05 17:37:01 +0100

    Add stack bond, English garden wall bond

--- src/data/Bonds.ts
+++ src/data/Bonds.ts
@@ -40,10 +40,42 @@ export const ENGLISH_BOND = (brick: BaseBrick): Bond => ({
   ],
 });
 
+export const STACK_BOND = (brick: BaseBrick): Bond => ({
+  courses: [
+    {
+      offsetFraction: 0,
+      bricks: [wholeBrick(brick)],
+    },
+  ],
+});
+
+export const ENGLISH_GARDEN_WALL_BOND = (brick: BaseBrick): Bond => ({
+  courses: [
+    {
+      offsetFraction: 0,
+      bricks: [cutBrick(brick, 1 / 2)],
+    },
+    {
+      offsetFraction: 1 / 4,
+      bricks: [wholeBrick(brick)],
+    },
+    {
+      offsetFraction: 3 / 4,
+      bricks: [wholeBrick(brick)],
+    },
+    {
+      offsetFraction: 1 / 4,
+      bricks: [wholeBrick(brick)],
+    },
+  ],
+});
+
 export const BOND_TYPES = {
   ['Flemish']: FLEMISH_BOND,
   ['Stretcher']: STRETCHER_BOND,
   ['English']: ENGLISH_BOND,
+  ['Stack']: STACK_BOND,
+  ['English Garden Wall']: ENGLISH_GARDEN_WALL_BOND,
 };
 
 export type BondType = keyof typeof BOND_TYPES;
